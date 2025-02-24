cb7f1c99519acd5615deac27ca129fea21cfff9b
Manish Rathod <163328294+manishrathod2805@users.noreply.github.com>
2025-02-24 21:39:17 +0530

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,23 @@
+const contacts = [
+  {
+    name: "Zoro",
+    imgURL:
+      "https://wallpapers.com/images/hd/young-chibi-art-anime-zoro-pfp-2qi0vn1gb279jutr.jpg",
+    phone: "+13452728",
+    email: "lostzoro@gmail.com",
+  },
+  {
+    name: "Luffy",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU728qyBbsP_3S2RMPqwA7cgyIOWDbs1Pu8Q&s",
+    phone: "+98725256",
+    email: "sungodluffy@gmail.com",
+  },
+  {
+    name: "Sanji",
+    imgURL: "https://cdn.myanimelist.net/images/characters/5/136769.jpg",
+    phone: "+918 372 574",
+    email: "vinsmokesanji@gmail.com",
+  },
+];
+export default contacts;
